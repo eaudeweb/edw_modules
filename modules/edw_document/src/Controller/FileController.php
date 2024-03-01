e80e3370805a941ed627a0769ee105e4bcade5b6
Anthelixes <dani.eremia@yahoo.com>
2024-03-01 13:51:45 +0200

    [edw_document] Process already encoded URLs

--- modules/edw_document/src/Controller/FileController.php
+++ modules/edw_document/src/Controller/FileController.php
@@ -6,6 +6,8 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\Exception\FileNotExistsException;
+use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Drupal\Core\Url;
 use Drupal\edw_document\Services\DocumentManager;
 use Drupal\file\FileInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -33,16 +35,26 @@ class FileController extends ControllerBase implements ContainerInjectionInterfa
   protected $fileStorage;
 
   /**
+   * The path processor service.
+   *
+   * @var \Drupal\Core\PathProcessor\InboundPathProcessorInterface
+   */
+  protected $pathProcessor;
+
+  /**
    * Constructs a new FileController object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\edw_document\Services\DocumentManager $document_manager
    *   The document manager service.
+   * @param \Drupal\Core\PathProcessor\InboundPathProcessorInterface $path_processor
+   *   The inbound path processor.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, DocumentManager $document_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, DocumentManager $document_manager, InboundPathProcessorInterface $path_processor) {
     $this->fileStorage = $entity_type_manager->getStorage('file');
     $this->documentManager = $document_manager;
+    $this->pathProcessor = $path_processor;
   }
 
   /**
@@ -51,7 +63,8 @@ class FileController extends ControllerBase implements ContainerInjectionInterfa
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('edw_document.document.manager')
+      $container->get('edw_document.document.manager'),
+      $container->get('path_processor_manager')
     );
   }
 
@@ -65,8 +78,12 @@ class FileController extends ControllerBase implements ContainerInjectionInterfa
    *
    * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
    *   The response object for serving the file.
+   *
+   * @SuppressWarnings(PHPMD.UnusedFormalParameter)
    */
   public function serveFile(Request $request, $uuid) {
+    $url = $this->processPath($request);
+    $uuid = $url->getRouteParameters()['uuid'];
     $files = $this->fileStorage->loadByProperties([
       'uuid' => $uuid,
     ]);
@@ -84,6 +101,20 @@ class FileController extends ControllerBase implements ContainerInjectionInterfa
     $response = new BinaryFileResponse($uri, 200, $headers);
     $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, $filename);
     return $response;
+  }
+
+  /**
+   * Processes the path for the given request.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object.
+   *
+   * @return \Drupal\Core\Url
+   *   The processed URL object.
+   */
+  protected function processPath($request) {
+    $path = $this->pathProcessor->processInbound($request->getPathInfo(), $request);
+    return Url::fromUserInput($path, ['query' => $request->query->all()]);
   }
 
 }
