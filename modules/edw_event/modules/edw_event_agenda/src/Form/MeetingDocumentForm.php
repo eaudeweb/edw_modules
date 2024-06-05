9edfe17c1e38174ec113cfeca973525d72a8b180
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2024-06-05 16:32:09 +0300

    Refs #23709 - scroll to correct agenda

--- modules/edw_event/modules/edw_event_agenda/src/Form/MeetingDocumentForm.php
+++ modules/edw_event/modules/edw_event_agenda/src/Form/MeetingDocumentForm.php
@@ -82,7 +82,13 @@ class MeetingDocumentForm implements ContainerInjectionInterface {
   public function formRedirect(array &$form, FormStateInterface $form_state) {
     $meetingId = $this->currentRequest->get('nid');
     $phase = $form_state->getUserInput()['field_document_phase'] ?? $this->currentRequest->get('field_document_phase');
-    $form_state->setRedirect("edw_event.documents.$phase", ['node' => $meetingId]);
+    $agendaId = $this->currentRequest->get('field_agenda');
+
+    $url_options = [
+      'fragment' => "$agendaId",
+    ];
+
+    $form_state->setRedirect("edw_event.documents.$phase", ['node' => $meetingId], $url_options);
   }
 
 }
