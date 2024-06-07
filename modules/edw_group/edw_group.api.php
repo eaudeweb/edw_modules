439ffd2c30c8a27d77f32f3623f776107b0b3c53
Anthelixes <dani.eremia@yahoo.com>
2024-06-07 12:05:30 +0300

    [edw_group] Include hooks file

---
+++ modules/edw_group/edw_group.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Hooks and documentation related to EDW group module.
+ */
+
+/**
+ * Updates private roles for node grants system.
+ *
+ * @param array $privateRoles
+ *   Array with private roles.
+ *
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+function hook_private_access_roles(&$privateRoles) {
+
+}
