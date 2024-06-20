e2fd6025fb0d01b1437b31b27178c2d66b73a82c
Anthelixes <dani.eremia@yahoo.com>
2024-06-20 09:34:19 +0300

    Temporary remove redirect to 'edw_event.documents.in_session.document_type' route

--- modules/edw_event/modules/edw_event_agenda/src/Form/MeetingDocumentForm.php
+++ modules/edw_event/modules/edw_event_agenda/src/Form/MeetingDocumentForm.php
@@ -87,11 +87,6 @@ class MeetingDocumentForm implements ContainerInjectionInterface {
     $meetingId = $this->currentRequest->get('nid');
     $phase = $this->currentRequest->get('field_document_phase');
     $phase = $this->getDocumentPhase($phase, $form_state);
-    $documentType = $this->currentRequest->get('field_document_types');
-    if (isset($documentType) && $phase == 'in_session') {
-      $form_state->setRedirect("edw_event.documents.in_session.document_type", ['node' => $meetingId]);
-      return;
-    }
     $form_state->setRedirect("edw_event.documents.$phase", ['node' => $meetingId]);
   }
 
