6c28fc8fd8ac69ab45ed80baabd4668a3994c606
Anthelixes <dani.eremia@yahoo.com>
2024-07-10 15:33:52 +0300

    refs #23709 - refactoring and extends the logic to open agenda by phase

--- modules/edw_themes/modules/edwt_backend/js/in-session-agendas.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const accordionId = document.location.hash.substring(1);
-	const accordion = document.getElementById('panel-in-session-by-' + accordionId);
-	if (accordion) {
-		accordion.classList.add('show');
-		accordion.scrollIntoView({behavior: "smooth"});
-	}
-});
