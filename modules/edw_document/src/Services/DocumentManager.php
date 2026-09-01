9d440718970f124ade8c4a5c3b7a0a5bc0bb88df
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2026-09-01 13:40:43 +0300

    Revert "refs #37907 - group documents per-extension"
    This reverts commit 235c6cf5662051421292e05cb31a631e42035caf.

--- modules/edw_document/src/Services/DocumentManager.php
+++ modules/edw_document/src/Services/DocumentManager.php
@@ -32,15 +32,6 @@ class DocumentManager {
     'video' => "VIDEO",
     'text' => "TEXT",
     'image' => "IMG",
-    'doc' => 'DOC',
-    'docx' => 'DOCX',
-    'rtf' => 'RTF',
-    'txt' => 'TXT',
-    'xls' => 'XLS',
-    'xlsx' => 'XLSX',
-    'docm' => 'DOCM',
-    'ppt' => 'PPT',
-    'pptx' => 'PPTX'
   ];
 
   protected $entityTypeId = 'node';
@@ -136,12 +127,17 @@ class DocumentManager {
   public function getUriType(string $uri) {
     $extensionMapping = [
       'csv' => 'document',
+      'doc' => 'document',
+      'docx' => 'document',
       'fodg' => 'document',
       'fodt' => 'document',
       'odf' => 'document',
       'odg' => 'document',
       'odt' => 'document',
       'pages' => 'document',
+      'rtf' => 'document',
+      'pdf' => 'pdf',
+      'txt' => 'text',
 
       'gif' => 'image',
       'jpg' => 'image',
@@ -152,10 +148,14 @@ class DocumentManager {
       'key' => 'presentation',
       'fodp' => 'presentation',
       'odp' => 'presentation',
+      'ppt' => 'presentation',
+      'pptx' => 'presentation',
 
       'numbers' => 'spreadsheet',
       'fods' => 'spreadsheet',
       'ods' => 'spreadsheet',
+      'xls' => 'spreadsheet',
+      'xlsx' => 'spreadsheet',
 
       'shtml' => 'link',
       'htm' => 'link',
@@ -163,17 +163,6 @@ class DocumentManager {
       'mp4' => 'video',
       'mov' => 'video',
       'avi' => 'video',
-
-      'doc' => 'doc',
-      'docx' => 'docx',
-      'pdf' => 'pdf',
-      'rtf' => 'rtf',
-      'txt' => 'txt',
-      'xls' => 'xls',
-      'xlsx' => 'xlsx',
-      'docm' => 'docm',
-      'ppt' => 'ppt',
-      'pptx' => 'pptx'
     ];
 
     $extension = pathinfo($uri, PATHINFO_EXTENSION);
@@ -410,10 +399,9 @@ class DocumentManager {
       return in_array($fileType, $availableFormats);
     }, ARRAY_FILTER_USE_KEY);
 
-    $keys = array_keys($icons);
-    return array_map(function ($key) {
-      return DocumentManager::ICONS_LABEL_INFO[$key] ?? '';
-    }, array_combine($keys, $keys));
+    return array_map(function ($icon) use ($icons) {
+      return !empty(array_search($icon, $icons)) ? DocumentManager::ICONS_LABEL_INFO[array_search($icon, $icons)] : '';
+    }, $icons);
   }
 
   /**
@@ -473,15 +461,6 @@ class DocumentManager {
       'image' => "$iconsPath/image-x-generic.png",
       'html' => "$iconsPath/text-html.png",
       'link' => "$iconsPath/text-html.png",
-      'doc' => "$iconsPath/x-office-document.png",
-      'docx' => "$iconsPath/x-office-document.png",
-      'rtf'=> "$iconsPath/x-office-document.png",
-      'txt' => "$iconsPath/text-plain.png",
-      'xls' => "$iconsPath/x-office-spreadsheet.png",
-      'xlsx' => "$iconsPath/x-office-spreadsheet.png",
-      'docm' => "$iconsPath/x-office-document.png",
-      'ppt' => "$iconsPath/x-office-presentation.png",
-      'pptx' => "$iconsPath/x-office-presentation.png",
     ];
   }
 
